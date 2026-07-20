3ca932a6545bfb995342a69fd7afded9fa307769
Lucas Mussatto Rodrigues <lucasmussatto71@gmail.com>
2026-07-20 19:53:37 -0300

    Arquivos iniciais

---
+++ src/main/java/com/devLucas/MeuFinanceiro/MeuFinanceiroApplication.java
@@ -0,0 +1,13 @@
+package com.devLucas.MeuFinanceiro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuFinanceiroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuFinanceiroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devLucas/MeuFinanceiro/MeuFinanceiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devLucas.MeuFinanceiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeuFinanceiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
